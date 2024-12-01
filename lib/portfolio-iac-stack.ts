026a9dc921b8afe39a9d54a97db283ff4d92cb06
tarp <travistarpley0@gmail.com>
2024-11-30 17:31:09 -0800

    feat: Added stack

--- lib/portfolio-iac-stack.ts
+++ lib/portfolio-iac-stack.ts
@@ -1,16 +1,51 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Distribution } from "aws-cdk-lib/aws-cloudfront";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as eventSources from "aws-cdk-lib/aws-lambda-event-sources";
+import { S3BucketOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { getConfig } from "./config";
 
 export class PortfolioIacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const config = getConfig();
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "DestinationBucket", {
+      bucketName: config.DEST_BUCKET_NAME,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      enforceSSL: true,
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PortfolioIacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const uploadBucket = new s3.Bucket(this, "uploadBucket", {
+      bucketName: "tarp-blog-upload-images",
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    const resizeImageLambda = new NodejsFunction(this, "ResizeImages", {
+      handler: "handler",
+      entry: "./src/index.ts",
+      runtime: lambda.Runtime.NODEJS_18_X,
+      architecture: lambda.Architecture.X86_64,
+      environment: {
+        DEST_BUCKET: config.DEST_BUCKET_NAME,
+      },
+    });
+    new Distribution(this, "BlogImageCache", {
+      defaultBehavior: {
+        origin: S3BucketOrigin.withOriginAccessControl(bucket),
+      },
+    });
+
+    const s3PutEventSource = new eventSources.S3EventSource(uploadBucket, {
+      events: [s3.EventType.OBJECT_CREATED],
+    });
+
+    resizeImageLambda.addEventSource(s3PutEventSource);
   }
 }
